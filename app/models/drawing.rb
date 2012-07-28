1e9263102b7ffc252307d49bb7d534896b93abcd
Mathieu Triay <mathieu.triay@gmail.com>
2012-07-28 14:28:06 +0100

    deleted base64 sub. done in JS instead

--- app/models/drawing.rb
+++ app/models/drawing.rb
@@ -10,7 +10,7 @@ class Drawing
   	embedded_in :user
 
   	def from_base64(image_data, user)
-  		self.image = Base64.decode64(image_data.sub("data:image/png;base64,", ""))
+  		self.image = Base64.decode64(image_data)
   		self.user = user
   	end
 end
