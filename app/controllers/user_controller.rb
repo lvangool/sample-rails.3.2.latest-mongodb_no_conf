1f475651a3b9c5b678051a7fae8824314a5fc06c
Mathieu Triay <mathieu.triay@gmail.com>
2012-08-03 15:27:42 +0100

    Hash formatting

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -32,14 +32,19 @@ class UserController < ApplicationController
 
 		if !@user.nil?
 			# Should get only the drawing type of mission
-			@result_hash = {status: "success", 
-							missions: @user.missions.map {|mission| {_id: mission._id, 
+			@result_hash = {
+							status: "success", 
+							missions: @user.missions.map {|mission| {
+																	_id: mission._id, 
 																	completed: mission.completed, 
 																	prompt: mission.prompt, 
 																	confirmation: mission.confirmation, 
 																	name: mission.name, 
-																	result: ({image: mission.result_drawing.image.url(format: 'png', _id: mission.result_drawing._id)}) if mission.result_drawing), 
-																	template: ({image: mission.template_drawing.image.url(format: 'png', _id: mission.template_drawing)}) if mission.template_drawing)} }}
+																	result: ({image: mission.result_drawing.image.url(format: 'png', _id: mission.result_drawing._id)} if mission.result_drawing), 
+																	template: ({image: mission.template_drawing.image.url(format: 'png', _id: mission.template_drawing)} if mission.template_drawing)
+																	} 
+														}	
+							}
 		else
 			@result_hash = {status: "failure"}
 		end
