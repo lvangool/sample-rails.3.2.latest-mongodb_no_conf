5a2c7412690e374a89f8622be5dbfbdb232322d2
Mathieu Triay <mathieu.triay@gmail.com>
2012-08-21 10:31:45 +0100

    new method to generate usernames + password. new add from mail

--- app/controllers/drawing_controller.rb
+++ app/controllers/drawing_controller.rb
@@ -1,7 +1,7 @@
 class DrawingController < ApplicationController
 
 	skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
-	
+
 	# Delete this block after testing
 	before_filter :allow_cross_domain_access
 	def allow_cross_domain_access
@@ -9,7 +9,31 @@ class DrawingController < ApplicationController
 	  response.headers["Access-Control-Allow-Methods"] = "*"
 	end
 	# end of block to delete after testing
-	
+
+	def add_from_email
+		if User.where(email: params[:email]).exists?
+			@user = User.find_by(email: params[:email])
+		else
+			@user = User.create_from_email(params[:email])
+		end
+
+		@drawing = @user.drawings.build
+		@drawing.strokes_attributes = JSON.parse(params[:curves].to_s)
+		@drawing.temp_image = params[:image]
+
+		respond_to do |format|
+			if @user.save && @drawing.save
+				Drawing.delay.process_drawing(@user._id, @drawing._id, params[:base_id])
+				UserMailer.delay.send_drawing(@user)
+
+				format.json { render json: { status: "success" } }
+			else
+				format.json { render json: { status: "failure" } }
+			end
+		end
+
+	end
+
 	def add_from_app
 		@user = User.find_for_token_authentication(params)
 		@drawing = nil
@@ -24,7 +48,6 @@ class DrawingController < ApplicationController
 		respond_to do |format|
 			if !@drawing.nil? && @drawing.save
 				Drawing.delay.process_drawing(@user._id, @drawing._id, params[:base_id])
-				UserMailer.delay.send_drawing
 
 				format.json { render json: { status: "success" } }
 			else
