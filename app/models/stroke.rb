2f28b42bce060e5e58d5a05ab6ab6d9d70a11445
Mathieu Triay <mathieu.triay@gmail.com>
2012-08-30 17:08:43 +0100

    test to convert point to array

--- app/models/stroke.rb
+++ app/models/stroke.rb
@@ -5,9 +5,9 @@ class Stroke
   field :opacity
   field :size
   field :eraser
+  field :sample_points, type: Array
 
   embedded_in :drawing
-  embeds_many :sample_points, class_name: "Point", inverse_of: :strokes
 
   accepts_nested_attributes_for :sample_points
 end
